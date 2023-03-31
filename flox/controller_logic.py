bac680c506313e213657a7fce6b473ae2b076e90
Nathaniel Hudson <nchudson88@gmail.com>
2023-03-31 00:22:22 -0500

    RedisConnector -> EndpointConnector (needs testing)

--- flox/controller_logic.py
+++ flox/controller_logic.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 import time
 
 from collections import defaultdict
@@ -7,9 +8,42 @@ from flox.logger import *
 from flox.results import FloxResults
 from funcx.sdk.executor import FuncXExecutor
 from pandas import DataFrame
+from pathlib import Path
+from proxystore.connectors.endpoint import EndpointConnector
+from proxystore.store import Store
 from tensorflow import keras
 from time import perf_counter
 from typing import Optional
+
+
+def _federated_fit_round():
+    pass
+
+
+def _init_store(**store_args) -> tuple[Optional[Store], bool]:
+    args = ["store_name", "endpoint_ids", "endpoint_ids", "store_dir", "metrics"]
+    req_args = args[:-2]
+    use_proxystore = any([store_args.get(arg, None) is None for arg in req_args])
+    if use_proxystore:
+        store = Store(
+            name=store_args["store_name"],
+            connector=EndpointConnector(
+                endpoints=store_args["endpoint_ids"],
+                proxystore_dir=store_args.get("store_dir", None)
+            ),
+            metrics=store_args.get("metrics", True)
+        )
+    else:
+        store = None
+    return store, use_proxystore
+
+
+def _confirm_local_execution(endpoint_ids: list[str]) -> bool:
+    local_endpoints = list(filter(lambda val: val.startswith("local"), endpoint_ids))
+    if not (len(local_endpoints) == 0 or len(local_endpoints) == len(endpoint_ids)):
+        raise ValueError("You cannot have endpoints of mixed types. You can only have all "
+                         "local endpoints (prefixed with 'local') or all funcX endpoints.")
+    return all([endp.startswith("local") for endp in endpoint_ids])
 
 
 def federated_fit(
@@ -32,7 +66,7 @@ def federated_fit(
         y_test=None,
         store_args: dict[str, str] = None,  # ProxyStore Store object
         history_metrics_aggr: str = None,
-        silent: bool = False
+        silent: bool = True
 ) -> DataFrame:
     # Handle default argument values.
     if isinstance(num_samples, int):
@@ -40,7 +74,7 @@ def federated_fit(
     if isinstance(epochs, int):
         epochs = [epochs] * len(endpoint_ids)
     if path_dir is None:
-        path_dir = "/home/pi/datasets"
+        path_dir = Path("/home/pi/datasets")
     if isinstance(path_dir, str):
         path_dir = [path_dir] * len(endpoint_ids)
     if history_metrics_aggr is None:
@@ -55,6 +89,9 @@ def federated_fit(
         raise ValueError(f"Length of iterators ('endpoint_ids', 'num_samples', "
                          f"'epochs', 'path_dir') must be of the same length.")
 
+    # Identify the execution kind.
+    locally_execute = _confirm_local_execution(endpoint_ids)
+
     # Initialize the data indices for each of the training endpoints.
     (x_train, _), (_, _) = keras.datasets.cifar10.load_data()
     train_indices = {
@@ -62,29 +99,25 @@ def federated_fit(
         for endp, k in zip(endpoint_ids, num_samples)
     }
 
-    # Global Federated training loop.
+    # Initialize the store to be used (if specified) and the results dictionary
+    # for the entire experiment.
+    store, use_proxystore = _init_store(**store_args)
     results = defaultdict(list)
+
+    # Global Federated training loop.
     for rnd in range(loops):
-        # Save the model architecture, weights, and prepare to store tasks/results for execution.
+        # Save the model architecture, weights, and prepare to store tasks/results
+        # for execution.
         model_json = global_model.to_json()
-        global_model_weights = np.asarray(global_model.get_weights(), dtype=object)
-        tasks, local_results = [], []
+        global_model_weights = np.asarray(global_model.get_weights())  # , dtype=object)
+        tasks, local_results, endp_end_times = list(), list(), dict()
 
-        # Identify the execution kind.
-        local_endpoints = list(filter(lambda val: val.startswith('local'), endpoint_ids))
-        if not (len(local_endpoints) == 0 or len(local_endpoints) == len(endpoint_ids)):
-            raise ValueError("You cannot have endpoints of mixed types. You can only have all "
-                             "local endpoints (prefixed with 'local') or all funcX endpoints.")
-        locally_execute = all([endp.startswith("local") for endp in endpoint_ids])
-
-        # submit the corresponding parameters to each endpoint for a round of FL
-        logging.info(f'{iters=}')
-        endpoint_end_times = {}
         for endp, samples, n_epochs, path in zip(*iters):
+            weights = store.proxy(global_model_weights) if use_proxystore else global_model_weights
             kwargs = dict(
                 endpoint_id=endp,
                 json_model_config=model_json,
-                global_model_weights=global_model_weights,
+                global_model_weights=weights,
                 train_indices=train_indices[endp],
                 epochs=n_epochs,
                 keras_dataset=keras_dataset,
@@ -96,14 +129,11 @@ def federated_fit(
                 loss=loss,
                 optimizer=optimizer,
                 metrics=metrics,
-                store_kind="Redis",
-                store_name=store_args["name"],
-                store_hostname=store_args["hostname"],
-                store_port=store_args["port"],
+                use_proxystore=use_proxystore
             )
             if locally_execute:
                 local_results.append(local_fit(**kwargs))
-                endpoint_end_times[endp] = perf_counter()
+                endp_end_times[endp] = perf_counter()
             else:
                 with FuncXExecutor(endp) as fx:
                     tasks.append(fx.submit(local_fit, **kwargs))
@@ -113,7 +143,7 @@ def federated_fit(
             for t in tasks:
                 res = t.result()
                 local_results.append(res)
-                endpoint_end_times[res["endpoint_id"]] = perf_counter()
+                endp_end_times[res["endpoint_id"]] = perf_counter()
 
         # Extract model updates from endpoints and then aggregate them to update the global model.
         model_weights = [res["model_weights"] for res in local_results]
@@ -131,11 +161,14 @@ def federated_fit(
             else:  # i.e., history_metrics_aggr == "recent"
                 acc_result = res["accuracy"][-1]
                 loss_result = res["loss"][-1]
+
             results["round"].append(rnd)
             results["endpoint_id"].append(res["endpoint_id"])
             results["accuracy"].append(acc_result)
             results["loss"].append(loss_result)
-            results["transfer_time"].append(endpoint_end_times[res["endpoint_id"]] - res["time_before_transfer"])
+            results["transfer_time"].append(endp_end_times[res["endpoint_id"]] - res["time_before_transfer"])
+            results["model_size_bytes"].append(sys.getsizeof(global_model_weights))
+            results["local"].append(locally_execute)
 
         # Evaluate the global model, if all the necessary parameters are given.
         if all([x_test is not None, y_test is not None]):
@@ -143,7 +176,7 @@ def federated_fit(
             if not silent:
                 logging.info(f"Test loss: {test_loss}  |  Test accuracy: {test_acc}")
 
-        # If `time_interval` is supplied, wait for `time_interval` seconds
+        # If `time_interval` is supplied, wait for `time_interval` seconds.
         if time_interval > 0:
             time.sleep(time_interval)
 
--- flox/endpoint_logic.py
+++ flox/endpoint_logic.py
@@ -13,11 +13,7 @@ def local_fit(
         loss="sparse_categorical_crossentropy",
         optimizer="adam",
         metrics=None,
-        # Arguments for ProxyStore Store object
-        store_kind='Redis',
-        store_name=None,
-        store_hostname=None,
-        store_port=None,
+        use_proxystore: bool = False
 ):
     # Import all the dependencies required for funcX, TensorFlow, and ProxyStore
     import numpy as np
@@ -26,42 +22,43 @@ def local_fit(
     from tensorflow import keras
     from time import perf_counter
     from proxystore.connectors.redis import RedisConnector
+    from proxystore.proxy import is_resolved, Proxy
     from proxystore.store import get_store, register_store, Store
 
     if metrics is None:
         metrics = ["accuracy"]
 
     # retrieve (and optionally process) the data
-    AVAILABLE_DATASETS = [
-        'mnist', 'fashion_mnist', 'cifar10', 'cifar100',
-        'imdb', 'reuters', 'boston_housing'
-    ]
-    DATASET_MAP = {
-        'mnist': keras.datasets.mnist,
-        'fashion_mnist': keras.datasets.fashion_mnist,
-        'cifar10': keras.datasets.cifar10,
-        'cifar100': keras.datasets.cifar100,
-        'imdb': keras.datasets.imdb,
-        'reuters': keras.datasets.reuters,
-        'boston_housing': keras.datasets.boston_housing
+    dataset_map = {
+        "mnist": keras.datasets.mnist,
+        "fashion_mnist": keras.datasets.fashion_mnist,
+        "cifar10": keras.datasets.cifar10,
+        "cifar100": keras.datasets.cifar100,
+        "imdb": keras.datasets.imdb,
+        "reuters": keras.datasets.reuters,
+        "boston_housing": keras.datasets.boston_housing
     }
-    IMG_DATASETS = ['mnist', 'fashion_mnist', 'cifar10', 'cifar100']
+    img_datasets = ["mnist", "fashion_mnist", "cifar10", "cifar100"]
+
+    if use_proxystore:
+        assert isinstance(global_model_weights, bytes) and isinstance(global_model_weights, Proxy)
+        assert not is_resolved(global_model_weights)
+        store = get_store(global_model_weights)
+        if store is None:
+            raise RuntimeError("Cannot find ProxyStore backend to use.")
+    else:
+        store = None
 
     ####################################################################################################################
     ####################################################################################################################
 
-    # check if the Keras dataset exists
-    if keras_dataset not in AVAILABLE_DATASETS:
-        raise Exception(f"Please select one of the built-in Keras datasets: {AVAILABLE_DATASETS}")
-
-    # load the data
-    (x_train, y_train), _ = DATASET_MAP[keras_dataset].load_data()
-
-    # take a random set of images
+    # Load the data with the indices specified for this client.
+    if keras_dataset not in dataset_map:
+        raise Exception(f"Please select one of the built-in Keras datasets: {list(dataset_map)}")
+    (x_train, y_train), _ = dataset_map[keras_dataset].load_data()
     x_train = x_train[train_indices]
     y_train = y_train[train_indices]
-
-    if preprocess and keras_dataset in IMG_DATASETS:
+    if preprocess and keras_dataset in img_datasets:
         num_classes = 100 if keras_dataset == "cifar100" else 10
         x_train = x_train.astype("float32") / 255
         if x_train.shape[-1] not in [1, 3]:
@@ -79,43 +76,18 @@ def local_fit(
 
     # Train the model on the local data and extract the weights then convert them to numpy.
     history = model.fit(x_train, y_train, epochs=epochs)
-    print(f'{type(history)=}\n{history.history.keys()}')
-    model_weights = model.get_weights()
-    np_model_weights = np.asarray(model_weights, dtype=object)
+    model_weights = np.asarray(model.get_weights(), dtype=object)
 
     # Return the updated weights and number of samples the model was trained on
-    store_args = [store_name, store_port, store_hostname]
     data = {
         "endpoint_id": endpoint_id,
-        "model_weights": np_model_weights,
+        "model_weights": model_weights,
         "samples_count": x_train.shape[0],
         "accuracy": history.history["accuracy"],
         "loss": history.history["loss"],
         "num_data_samples": len(train_indices),
-        "data_transfer_size": float(0)
+        "data_transfer_size": float(0),
+        "time_before_transfer": perf_counter()
     }
     data["data_transfer_size"] = sys.getsizeof(pickle.dumps(data))
-    if all([arg is None for arg in store_args]):
-        data["time_before_transfer"] = perf_counter()
-
-        return data
-    elif all([arg is not None for arg in store_args]):
-        data["time_before_transfer"] = perf_counter()
-        store = Store(
-            name=store_name,
-            connector=RedisConnector(hostname=store_hostname, port=store_port),
-            metrics=True
-        )
-        """
-        # Endpoint version
-        store = Store(
-            name=store_name,
-            connector=EndpointConnector(hostname=store_hostname, port=store_port),
-            metrics=True
-        )
-        """
-        proxy = store.proxy(data)
-        return proxy
-    else:
-        raise ValueError(f'Must either have ALL store-based params (i.e., {store_args}) '
-                         f'be None or you must give them ALL a non-None value.')
+    return store.proxy(data) if use_proxystore else data
